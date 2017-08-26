a0fd42b4fe994dc85b2afb76fc409040a059ffc9
unknown <kiennd@hotmail.com>
2017-08-27 03:23:03 +0700

    them xu ly ve states

--- public/xuly.js
+++ public/xuly.js
@@ -5,20 +5,36 @@ var Chao = React.createClass({
   hienthongbao:function(){
       alert(this.props.ten);
   },
+  themHocVien(){
+    this.state.tongHocVien = parseInt(this.state.tongHocVien) +1;
+      this.setState(this.state);
+  },
+  //cach 2
+  //themHocVien(){
+  //    this.setState({tongHocVien: parseInt(this.state.tongHocVien) + 1});
+  //},
+  //Vi du ve state trong React
+  //--------------------------------------------------//
+  getInitialState(){
+    return {tongHocVien: this.props.tongHocVien};
+  },
+  //ham render trong react
   render:function(){
     return(
       <div>
       <h1 className = "mauvang" >{this.props.ten}</h1>
-      <h1 className = "mauxanh" >{this.props.name}<p>{this.props.children}</p>{this.props.nghenghiep}</h1>
+      <div>so hoc vien la:{this.state.tongHocVien}</div>
       <button onClick={()=>getname(this.props.ten)}>hien thong bao</button>
+      <button onClick={this.themHocVien}>Them</button>
       </div>
     );
   }
 });
 ReactDOM.render(
   <div>
-  <Chao ten="dung cho"/>
-  <Chao name="phoc" nghenghiep="that nghiep">maria</Chao>
+  <Chao ten="BMW" tongHocVien="10"/>
+  <Chao ten="toyota" tongHocVien="20">maria</Chao>
+  <p>-----------------------------------------------------------</p>
   <h1 className = "mauvang"> Hello mac dinh </h1>
   </div>
   , document.getElementById('root'));
