49e2c67bb1c1783c378916fb0bb4345cbba31627
unknown <kiennd@hotmail.com>
2017-08-27 21:58:44 +0700

    bai-tap-ve-state

--- public/xuly.js
+++ public/xuly.js
@@ -1,67 +1,21 @@
-function getname(name){
-  return alert(name);
-}
-var Chao = React.createClass({
-  hienthongbao:function(){
-      alert(this.props.ten);
-
-      
+var Clickinc = React.createClass({
+  tangso(){
+    this.state.soHientai = parseInt(this.state.soHientai) +1 ;
+    this.setState(this.state);
   },
-  themHocVien(){
-    this.state.tongHocVien = parseInt(this.state.tongHocVien) +1;
-      this.setState(this.state);
-  },
-  //cach 2
-  //themHocVien(){
-  //    this.setState({tongHocVien: parseInt(this.state.tongHocVien) + 1});
-  //},
-  //Vi du ve state trong React
-  //--------------------------------------------------//
   getInitialState(){
-    return {tongHocVien: this.props.tongHocVien};
+    return{soHientai: this.props.soHientai};
   },
-  //ham render trong react
   render:function(){
-    return(
-      <div>
-      <h1 className = "mauvang" >{this.props.ten}</h1>
-      <div>so hoc vien la:{this.state.tongHocVien}</div>
-      <button onClick={()=>getname(this.props.ten)}>hien thong bao</button>
-      <button onClick={this.themHocVien}>Them</button>
-      </div>
-    );
-  }
-});
-var InputTag = React.createClass(
-  {
-    show(){
-      var text = this.refs.txt.value;
-      var sl = this.refs.sl.value;
-      alert(text);
-      alert(sl);
-    },
-    render(){
-      return (
+      return(
         <div>
-              <select ref="sl">
-              <option value="a">aaaa</option>
-              <option value="b">bbbb</option>
-              <option value="c">cccc</option>
-              </select>
-
-              <input type="text" ref='txt'/>
-              <button onClick={this.show}>hien thi</button>
+          <button onClick={this.tangso}>Hello {this.state.soHientai}</button>
         </div>
       )
     }
-
 });
 ReactDOM.render(
   <div>
-  <InputTag />
-  <Chao ten="BMW" tongHocVien="10"/>
-  <Chao ten="toyota" tongHocVien="20">maria</Chao>
-  <p>-----------------------------------------------------------</p>
-  <h1 className = "mauvang"> Hello mac dinh </h1>
+<Clickinc soHientai="10" />
   </div>
   , document.getElementById('root'));
