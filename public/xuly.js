8bfb165a45692746ae418f9f1f632cf4e379b2c5
unknown <kiennd@hotmail.com>
2017-08-28 03:48:16 +0700

    doi hinh anh bang onClick don gian

--- public/xuly.js
+++ public/xuly.js
@@ -14,8 +14,47 @@ var Clickinc = React.createClass({
       )
     }
 });
+
+var Album = React.createClass({
+  getInitialState(){
+    return{hinh: 1};
+  },
+  TangLenMot(){
+    this.setState(()=>{if (this.state.hinh>3) {
+      this.state.hinh =1;
+      this.setState(this.state);
+
+    } else {
+      this.state.hinh = parseInt(this.state.hinh) +1;
+      this.setState(this.state);
+    }});
+  },
+  GiamXuongMot(){
+    this.setState(()=>{if (this.state.hinh<2) {
+      this.state.hinh = 4;
+      this.setState(this.state);
+
+    } else {
+      this.state.hinh = parseInt(this.state.hinh) - 1;
+      this.setState(this.state);
+    }});
+},
+  render(){
+    return (
+
+      <div className="div-album">
+        <img src={"image/"+this.state.hinh +".jpg"} />
+        <button onClick={this.GiamXuongMot}>Quay lai</button>
+        <button onClick={this.TangLenMot}>Tiep theo</button>
+      </div>
+    );
+  }
+}
+);
+
 ReactDOM.render(
   <div>
 <Clickinc soHientai="10" />
+<Album />
   </div>
   , document.getElementById('root'));
