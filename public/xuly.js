21ae5fa2faeec37a0769a9be970cb61e7b376eab
unknown <kiennd@hotmail.com>
2017-08-30 18:48:57 +0700

    ung-dung-ghi-chu

--- public/xuly.js
+++ public/xuly.js
@@ -1,42 +1,46 @@
-const friends = [
-    {id:1, name: 'Dave',age:50},
-    {id:2,name: 'Kellie',age:42},
-    {id:3,name: 'Max',age:12},
-    {id:2,name: 'Jack',age:12}
-];
-var Note = React.createClass
-(
-  {
-  render()
-    {
-      return (
-        <p>hello my friend</p>
-              );
-    }
+function addDiv(){
+alert(123);
+};
+var Note = React.createClass({
+  render: function(){
+    return (<div className="div-note">
+            {this.props.children}
+    </div>);
   }
-);
-var List = React.createClass(
-  {
-    getInitialState(){
-      return({mang: friends});
-    },
-    add(){
-      this.state.mang.push("node js");
-      this.setState(this.state);
-    },
+});
+var List = React.createClass({
+  getInitialState(){
+    return {mang:["vip1","vip2","vip3"]}
+  },
+  render(){
+    return(
+    <div className='div-list'>
+      <button onClick={addDiv}>Them</button>
+      <h1> Hello world !</h1>
+      {
+        this.state.mang.map((note,chiso)=><Note key={chiso}>{note}</Note>)
+      }
+    </div>
+    )
+  }
+});
+
+var InputDiv = React.createClass({
+  send(){
+
+  },
   render(){
     return(
       <div>
-      <button onClick={this.add}>Them</button>
-      {
-        this.state.mang.map((note,chiso)=><h1 key={chiso}>{note.id}-{note.name}</h1>)
-      }
+        <input type="text" ref="txt" placeholder="enter your note"></input>
+        <button onClick={this.send}>Gui</button>
       </div>
     )
   }
 });
 ReactDOM.render(
   <div>
-  <List/>
+    <List/>
+    <InputDiv />
   </div>
   , document.getElementById('root'));
